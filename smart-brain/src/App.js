c4f832dffd0e324e37fbd040e24cb1c40a6fa2c1
hilaryyates <hilryyates@gmail.com>
2022-10-13 22:01:52 -0400

    style new bg

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -3,10 +3,20 @@ import Navigation from "./Navigation/Navigation";
 import Logo from "./Logo";
 import ImageLinkForm from "./ImageLinkForm";
 import Rank from "./Rank";
+// import React, { useState } from "react";
 
 function App() {
+  // const [input, setInput] = useState("");
+  // const onInputChange = (event) => {
+  //   event.preventDefault();
+  // };
   return (
     <div className='App'>
+      <div class='lines'>
+        <div class='line'></div>
+        <div class='line'></div>
+        <div class='line'></div>
+      </div>
       <Navigation />
       <Rank />
       <Logo />
--- smart-brain/src/ImageLinkForm.js
+++ smart-brain/src/ImageLinkForm.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ImageLinkForm = () => {
+  const [input, setInput] = useState("");
+  const onInputChange = (event) => {
+    event.preventDefault();
+    setInput(event.target.value);
+  };
   return (
     <div>
-      <p className='f3 main-text'>
+      <p className='f4 main-text'>
         {"This magic brain will detect faces in your pictures. Give it a try."}
       </p>
       <div class='form-box'>
-        <input className='f4 pa1 w-70 Zzenter' type='text' />
+        <input
+          onChange={onInputChange}
+          className='f4 pa1 w-70 Zzenter'
+          type='text'
+        />
         <button className='w-30 center grow f4 link ph3 pv2 white bg-ligh-purple'>
           Detect
         </button>
